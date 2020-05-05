35cc21414fc80281124e428913865fa11eff07e0
Juan Lino Perez Jr <juanlino@gmail.com>
2020-05-04 23:43:54 -0500

    jquery update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+
+    $('#hideimage').click(function () {
+
+        $("#heroimage").fadeOut(2500);
+        $("#changeText").addClass('largerText');
+
+})
+  setTimeout(function(){
+    $("#changeText").addClass('largerText');
+
+
+  })
+
+})
